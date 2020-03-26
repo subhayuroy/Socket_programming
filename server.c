11adfa99c6fb1b4f7d0a532f911ced27bb059bbb
subhayuroy <subhayu.roy2001@gmail.com>
2020-03-26 10:47:45 +0530

    Updating server.c

--- server.c
+++ server.c
@@ -17,12 +17,12 @@ int main(int argc, char const *argv[])
     int addrlen = sizeof(address);
     char buffer[1024]= {0};
     char *hello= "Hello from server";
-    if((server_fd= socket(AF_INET, SOCK_STREAM,0))==0)
+    if((server_fd= socket(AF_INET, SOCK_STREAM,0))==0)  //Creating socket file descriptor
     {
         perror("Socket failed");
         exit(EXIT_FAILURE);
     }
-    if(setsockopt(server_fd,SOL_SOCKET,SO_REUSEADDR|SO_REUSEPORT, &opt, sizeof(opt)))
+    if(setsockopt(server_fd,SOL_SOCKET,SO_REUSEADDR|SO_REUSEPORT, &opt, sizeof(opt)))  //Forcefully attaching socket to the PORT 8080
     {
         perror("Setsockopt");
         exit(EXIT_FAILURE);
@@ -30,7 +30,7 @@ int main(int argc, char const *argv[])
     address.sin_family = AF_INET;
     address.sin_addr.s_addr= INADDR_ANY;
     address.sin_port= htons(PORT);
-    if(bind(server_fd,(struct sockaddr *)&address, sizeof(address))<0)
+    if(bind(server_fd,(struct sockaddr *)&address, sizeof(address))<0)  //Forcefully attaching socket to the PORT 8080
     {
         perror("Build failed");
         exit(EXIT_FAILURE);
